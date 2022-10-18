4168d728477f7f563b7e8dd04021a6a701423d7e
simmons-wustl <dylan.simmons@wustl.edu>
2022-10-17 20:11:12 -0500

    Added Makefile.

--- category-software/Identify_Yourself/src/shell.cpp
+++ category-software/Identify_Yourself/src/shell.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <string>
 
 #ifndef USER
 #define USER "user"
@@ -9,26 +9,32 @@
 #define PASS "pass"
 #endif
 
-#define BUF_SIZE 1024
-
 int main(int argc, char* argv[])
 {
+   const unsigned int BUF_SIZE = 1024;
+   const std::string _user = USER;
+   const std::string _pass = PASS;
+   std::string inUser;
+   std::string inPass;
+   char buf[BUF_SIZE];
+
    if (argc != 2)
    {
       printf("Usage: ./shell <IP_addr>\n");
       return 1;
    }
-
-   char buf[BUF_SIZE];
-   memset(buf, 0, BUF_SIZE);
    
+   memset(buf, 0, BUF_SIZE);
    printf("Username: ");
+   scanf("%s", buf);
+   inUser = std::string(buf);
 
-   char* ret = fgets(buf, BUF_SIZE, stdin);
-   if (ret != NULL)
-   {
-      fputs(buf, stdout);
-   }
+   memset(buf, 0, BUF_SIZE);
+   printf("Password: ");
+   scanf("%s", buf);
+   inPass = std::string(buf);
+
+   printf("Username: %s, Password: %s\n", inUser.c_str(), inPass.c_str());
 
    return 0;
 }
