a477756a02e634ef15f591421afe5c0ae86808c2
simmons-wustl <dylan.simmons@wustl.edu>
2022-11-08 02:55:56 +0000

    Getting remote client IP address

--- category-network/Open_the_Flood_Gates/img_flask_app/entry.sh
+++ category-network/Open_the_Flood_Gates/img_flask_app/entry.sh
@@ -2,4 +2,4 @@
 
 #gunicorn --config=/ctf/gunicorn_conf.py app:app
 
-flask --app /ctf/server.py run --host=0.0.0.0
+flask --app /ctf/server.py --debug run --host=0.0.0.0
--- category-network/Open_the_Flood_Gates/img_flask_app/server.py
+++ category-network/Open_the_Flood_Gates/img_flask_app/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, send_file
+from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
-    return send_file("/ctf/index.html")
+   return str(request.remote_addr)
+   #return send_file("/ctf/index.html")
 
 if __name__ == "__main__":
    app.run("0.0.0.0")
