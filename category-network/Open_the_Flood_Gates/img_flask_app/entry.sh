ef0cfc4eec96a11327405dee664bdbf989ea461e
simmons-wustl <dylan.simmons@wustl.edu>
2022-11-09 01:55:07 +0000

    Got nginx reverse proxy to gunicorn/flask app working

--- category-network/Open_the_Flood_Gates/img_flask_app/entry.sh
+++ category-network/Open_the_Flood_Gates/img_flask_app/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#gunicorn --config=/ctf/gunicorn_conf.py app:app
+gunicorn -b 0.0.0.0:8000 ctf.server
 
-flask --app /ctf/server.py --debug run --host=0.0.0.0
+#flask --app /ctf/server.py --debug run --host=0.0.0.0
--- category-network/Open_the_Flood_Gates/img_flask_app/server.py
+++ category-network/Open_the_Flood_Gates/img_flask_app/server.py
@@ -1,11 +1,31 @@
+#!/usr/local/bin/python3
+
 from flask import *
+from scapy.all import *
+import time
+import random
+import socket
 
-app = Flask(__name__)
+srcIPAddr = socket.gethostbyname(socket.gethostname())
 
-@app.route("/")
+def sendICMPEcho(srcIP, dstIP):
+   #ip = IP(src = srcIP, dst = dstIP)
+   ip = IP(src = "10.0.0.2", dst = dstIP)
+   icmp = ICMP()
+   packet = ip/icmp/"THIS IS FROM flood.py"
+   send(packet)
+
+application = Flask(__name__)
+
+@application.route("/")
 def landing_page():
-   return str(request.remote_addr)
+   #srcIP = socket.gethostbyname(socket.gethostname())
+   #dstIP = request.remote_addr
+   #sendICMPEcho(srcIP, dstIP)
+   #return "Sent ICMP Echo to " + str(request.remote_addr)
+   return str(request.headers) + "\n" + str(request.host) + "\n" + str(request.remote_addr)
+   #return str(request.remote_addr)
    #return send_file("/ctf/index.html")
 
 if __name__ == "__main__":
-   app.run("0.0.0.0")
+   application.run("0.0.0.0")
