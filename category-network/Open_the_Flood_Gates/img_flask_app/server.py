5e56d5df19cb7e6a42e54eb3681a75996bc467d8
simmons-wustl <dylan.simmons@wustl.edu>
2022-11-11 01:12:10 +0000

    Got host network mode working, and changed PING to send reply rather than request

--- category-network/Open_the_Flood_Gates/img_flask_app/server.py
+++ category-network/Open_the_Flood_Gates/img_flask_app/server.py
@@ -3,33 +3,29 @@
 from flask import *
 from scapy.all import *
 from werkzeug.middleware.proxy_fix import ProxyFix
+from faker import Faker
 import time
 import random
-import socket
-
-
-#srcIPAddr = socket.gethostbyname(socket.gethostname())
-
-def sendICMPEcho(srcIP, dstIP):
-   ip = IP(src = srcIP, dst = dstIP)
-   icmp = ICMP()
-   packet = ip/icmp/"THIS IS FROM flood.py"
-   send(packet)
 
 application = Flask(__name__)
 
 application.wsgi_app = ProxyFix(application.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1) # https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
 
+fake = Faker()
+
+def send_icmp_reply(dstIP):
+   srcIP = fake.ipv4()
+   ip = IP(src = srcIP, dst = dstIP)
+   icmp = ICMP(type = 0) # Echo reply
+   packet = ip/icmp/"THIS IS FROM flood.py"
+   send(packet)
+
+
 @application.route("/")
 def landing_page():
-   srcIP = socket.gethostbyname(socket.gethostname())
-   dstIP = request.headers["X-Real-Ip"]
-   sendICMPEcho(srcIP, dstIP)
-   #return "Sent ICMP Echo to " + str(request.remote_addr)
-   #return str(request.headers) + str(request.host) + str(request.remote_addr)
-   #return str(request.remote_addr)
+   dstIP = request.headers["X-Forwarded-For"]
+   send_icmp_reply(dstIP)
    return send_file("/ctf/index.html")
-   #return str(request.headers["X-Real-Ip"])
 
 if __name__ == "__main__":
    application.run("0.0.0.0")
