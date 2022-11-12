0178fb5258b1957b8de92c9049c471ae76edb70f
simmons-wustl <dylan.simmons@wustl.edu>
2022-11-12 19:47:12 +0000

    Starting to define the logic for sending multiple packet types

--- category-network/Open_the_Flood_Gates/img_flask_app/server.py
+++ category-network/Open_the_Flood_Gates/img_flask_app/server.py
@@ -4,8 +4,12 @@ from flask import *
 from scapy.all import *
 from werkzeug.middleware.proxy_fix import ProxyFix
 from faker import Faker
-import time
 import random
+import os
+
+MAX_PACKETS_TO_SEND = 10
+
+flag = os.environ['CTF_FLAG']
 
 application = Flask(__name__)
 
@@ -13,18 +17,42 @@ application.wsgi_app = ProxyFix(application.wsgi_app, x_for=1, x_proto=1, x_host
 
 fake = Faker()
 
-def send_icmp_reply(dstIP):
-   srcIP = fake.ipv4()
-   ip = IP(src = srcIP, dst = dstIP)
+def craft_icmp_reply(dstIP):
+   ip = IP(src = fake.ipv4_public(), dst = dstIP)
    icmp = ICMP(type = 0) # Echo reply
-   packet = ip/icmp/"THIS IS FROM flood.py"
-   send(packet)
+   packet = ip/icmp
+   return packet
 
+def craft_dns_request(dstIP):
+   ip = IP(src = fake.ipv4_public(), dst = dstIP)
+   udp = UDP(dport = 53)
+   dns = DNS(qd = DNSQR(qname = fake.domain_name(), qtype = "A"))
+   packet = ip/udp/dns
+   return packet
+
+def flood_packets(dstIP):
+   random.seed()
+   packetWithFlag = random.randint(0, MAX_PACKETS_TO_SEND - 1)
+   
+   for i in range(MAX_PACKETS_TO_SEND):
+      packetToSend = random.choice(("ICMP", "DNS"))
+      
+      if packetToSend == "ICMP":
+         packetToSend = craft_icmp_reply(dstIP)
+      elif packetToSend == "DNS":
+         packetToSend = craft_dns_request(dstIP)
+       
+      if i == packetWithFlag:
+         packetToSend = packetToSend/flag 
+
+      send(packetToSend)
 
 @application.route("/")
 def landing_page():
    dstIP = request.headers["X-Forwarded-For"]
-   send_icmp_reply(dstIP)
+   #send_icmp_reply(dstIP)
+   #send_dns_request(dstIP)
+   flood_packets(dstIP)
    return send_file("/ctf/index.html")
 
 if __name__ == "__main__":
