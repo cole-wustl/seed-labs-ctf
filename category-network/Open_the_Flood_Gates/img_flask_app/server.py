cb83c3de49627f69c467db3bca0740f8b3ac09e3
simmons-wustl <dylan.simmons@wustl.edu>
2022-11-15 03:01:39 +0000

    Adding new logic to direct student to different pages

--- category-network/Open_the_Flood_Gates/img_flask_app/server.py
+++ category-network/Open_the_Flood_Gates/img_flask_app/server.py
@@ -18,9 +18,9 @@ application.wsgi_app = ProxyFix(application.wsgi_app, x_for=1, x_proto=1, x_host
 
 fake = Faker()
 
-def craft_icmp_reply(dstIP):
+def craft_icmp_request(dstIP):
    ip = IP(src = fake.ipv4_public(), dst = dstIP)
-   icmp = ICMP(type = 0) # Echo reply
+   icmp = ICMP(type = 8) # Echo request
    packet = ip/icmp
    return packet
 
@@ -31,31 +31,58 @@ def craft_dns_request(dstIP):
    packet = ip/udp/dns
    return packet
 
+def craft_tcp_syn(dstIP):
+   ip = IP(src = fake.ipv4_public(), dst = dstIP)
+   tcp = TCP(flags = 'S') # TCP SYN
+   packet = ip/tcp
+   return packet
+
 def flood_packets(dstIP):
    random.seed()
    packetWithFlag = random.randint(0, MAX_PACKETS_TO_SEND - 1)
 
    for i in range(MAX_PACKETS_TO_SEND):
-      packetToSend = random.choice(("ICMP", "DNS"))
+      packetToSend = random.choice(("ICMP", "DNS", "TCP"))
 
       if packetToSend == "ICMP":
-         packetToSend = craft_icmp_reply(dstIP)
+         packetToSend = craft_icmp_request(dstIP)
       elif packetToSend == "DNS":
          packetToSend = craft_dns_request(dstIP)
+      elif packetToSend == "TCP":
+         packetToSend = craft_tcp_syn(dstIP)
 
       if i == packetWithFlag:
          packetToSend = packetToSend/flag
 
       send(packetToSend)
 
+def flood_icmp_echo_request(dstIP):
+   MAX_ICMP_PACKETS = 1000
+   random.seed()
+   packetWithHint = random.randint(0, MAX_ICMP_PACKETS - 1)
+   for i in range(MAX_ICMP_PACKETS):
+      packet = craft_icmp_request(dstIP)
+      if i == packetWithHint:
+         packet = packet/("VISIT /sniff")
+      else:
+         packet = packet/(fake.binary(length = 64))
+         #packet = packet/(fake.sentence(nb_words = 10))
+      send(packet)
+
 @application.route("/")
-def landing_page():
+def index():
    dstIP = request.headers["X-Forwarded-For"]
-
-   theThread = threading.Thread(target = flood_packets, args = (dstIP,))
+   theThread = threading.Thread(target = flood_icmp_echo_request, args = (dstIP,))
    theThread.start()
-
    return send_file("/ctf/index.html")
+
+@application.route("/flag")
+def fake_flag():
+   return f"<h1> The flag is: {fake.word()} </h1>"
+
+@application.route("/<page>")
+def generic_page(page):
+   return f"<h1> Welcome to {page} </h1>\n<p> {fake.paragraph(nb_sentences = 10)} </p>"
 
 if __name__ == "__main__":
    application.run("0.0.0.0")
